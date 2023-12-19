90434a170a8bca0f21ff4cb188e24a1b4d30f52b
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2023-12-19 17:05:55 +0100

    fix: export the new classes

--- lib/classes/index.ts
+++ lib/classes/index.ts
@@ -4,5 +4,6 @@ import Games from "./games";
 import Clips from "./clips";
 import Users from "./users";
 import Search from "./search";
+import Streams from "./streams";
 
-export default { Twitch, Authentication, Games, Clips, Users, Search };
+export default { Twitch, Authentication, Games, Clips, Users, Search, Streams };
--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,16 @@
 import twitchApi from "./classes";
 
-const { Twitch, Users, Games, Clips, Authentication } = twitchApi;
+const { Twitch, Users, Games, Clips, Authentication, Search, Streams } =
+  twitchApi;
 
-export { Twitch as default, Twitch, Users, Games, Clips, Authentication };
+export {
+  Twitch as default,
+  Twitch,
+  Users,
+  Games,
+  Clips,
+  Authentication,
+  Search,
+  Streams,
+};
 export * from "./types";
